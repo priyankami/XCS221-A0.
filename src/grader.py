c6cd99a1930a115801ee1a3998c84ca34c4da7eb
Sean Fitzgerald <fitzgerald.sean.t@gmail.com>
2020-09-07 22:36:32 -0400

    Intial commit with first draft.

---
+++ src/grader.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python3
+import unittest, random, sys, copy, argparse, inspect, collections, os, pickle, gzip
+from graderUtil import graded, CourseTestRunner, GradedTestCase
+from collections import defaultdict
+
+# Import student submission
+import submission
+
+#############################################
+# HELPER FUNCTIONS FOR CREATING TEST INPUTS #
+#############################################
+def randvec():
+  v = defaultdict(float)
+  for _ in range(10):
+    v[random.randint(0, 10)] = random.randint(0, 10) - 5
+  return v
+
+def genSentence(K, L):
+  # K = alphabet size, L = length
+  return ' '.join(str(random.randint(0, K)) for _ in range(L))
+
+def genLimitedCharacterString(numChars, length):
+    # Generate a random string of the given length
+    return ' '.join(chr(random.randint(ord('a'), ord('a') + numChars - 1)) for _ in range(length))
+
+#########
+# TESTS #
+#########
+class Test_3a(GradedTestCase):
+
+  @graded()
+  def test_0(self):
+    """3a-0-basic:  Simple test case."""
+    self.assertEqual('word',
+                     submission.findAlphabeticallyLastWord('which is the last word alphabetically'))
+  
+  @graded()
+  def test_1(self):
+    """3a-1-basic:  Simple test case."""
+    self.assertEqual('sun',
+                     submission.findAlphabeticallyLastWord('cat sun dog')) 
+  
+  @graded()
+  def test_2(self):
+    """3a-2-basic:  Big test case."""
+    self.assertEqual('99999',
+                     submission.findAlphabeticallyLastWord(' '.join(str(x) for x in range(100000))))
+class Test_3b(GradedTestCase):
+  @graded()
+  def test_0(self):
+    """3b-0-basic:  Simple test case."""
+    self.assertEqual(5,
+                     submission.euclideanDistance((1, 5), (4, 1)))
+  @graded(is_hidden=True)
+  def test_1(self):
+    """3b-1-hidden:  100 random trials."""
+    random.seed(42)
+    for _ in range(100):
+      x1 = random.randint(0, 10)
+      y1 = random.randint(0, 10)
+      x2 = random.randint(0, 10)
+      y2 = random.randint(0, 10)
+      def comp(test_func):
+        test_func((x1, y1), (x2, y2))
+      self.compare_with_solution_or_wait(submission, 'euclideanDistance', comp)
+class Test_3c(GradedTestCase):
+  @graded()
+  def test_0(self):
+    """3c-0-basic:  Simple test case."""
+    self.assertEqual(sorted(['a a a a a']),
+                     sorted(submission.mutateSentences('a a a a a')))
+    self.assertEqual(sorted(['the cat']),
+                     sorted(submission.mutateSentences('the cat')))
+    self.assertEqual(sorted(['and the cat and the', 'the cat and the mouse', 'the cat and the cat', 'cat and the cat and']),
+                     sorted(submission.mutateSentences('the cat and the mouse')))
+
+  @graded(is_hidden=True)
+  def test_1(self):
+    """3c-1-hidden:  Small random trials."""
+    random.seed(42)
+    for _ in range(10):
+      sentence = genSentence(3, 5)
+      def comp(test_func):
+        test_func(sentence)
+      self.compare_with_solution_or_wait(submission, 'mutateSentences', comp)
+
+  @graded(is_hidden=True)
+  def test_2(self):
+    """3c-2-hidden:  Large random trials."""
+    random.seed(42)
+    for _ in range(10):
+      sentence = genSentence(25, 10)
+      def comp(test_func):
+        test_func(sentence)
+      self.compare_with_solution_or_wait(submission, 'mutateSentences', comp)
+class Test_3d(GradedTestCase):
+
+  @graded()
+  def test_0(self):
+    """3d-0-basic:  Simple test case."""
+    self.assertEqual(15,
+                     submission.sparseVectorDotProduct(defaultdict(float, {'a': 5}), defaultdict(float, {'b': 2, 'a': 3})))
+
+  @graded(is_hidden=True)
+  def test_1(self):
+    """3d-1-basic:  Random trials."""
+    random.seed(42)
+    for _ in range(10):
+      v1 = randvec()
+      v2 = randvec()
+      def comp(test_func):
+        test_func(v1, v2)
+      self.compare_with_solution_or_wait(submission, 'sparseVectorDotProduct', comp)
+class Test_3e(GradedTestCase):
+
+  @graded()
+  def test_0(self):
+    """3e-0-basic:  Simple test case."""
+    v = defaultdict(float, {'a': 5})
+    submission.incrementSparseVector(v, 2, defaultdict(float, {'b': 2, 'a': 3}))
+    self.assertEqual(defaultdict(float, {'a': 11, 'b': 4}), v, msg='Simple test.')
+
+  @graded(is_hidden=True)
+  def test_1(self):
+    """3e-1-hidden:  Random trials."""
+    random.seed(42)
+    for _ in range(10):
+      v1a = randvec()
+      v1b = v1a.copy()
+      v2 = randvec()
+      def comp(func):
+        func(v1b, 4, v2)
+        for key in list(v1b):
+          if v1b[key] == 0:
+            del v1b[key]
+        return v1b
+      self.compare_with_solution_or_wait(submission,
+                                         'incrementSparseVector',
+                                         comp)
+class Test_3f(GradedTestCase):
+
+  @graded()
+  def test_0(self):
+    """3f-0-basic:  Simple test case."""
+    self.assertEqual(set(['quick', 'brown', 'jumps', 'over', 'lazy']),
+                     submission.findSingletonWords('the quick brown fox jumps over the lazy fox'))
+
+  @graded(is_hidden=True)
+  def test_1(self):
+    """3f-1-basic:  Random trials."""
+    numTokens, numTypes = (1000, 10)
+    import random
+    random.seed(42)
+    text = ' '.join(str(random.randint(0, numTypes)) for _ in range(numTokens))
+    def comp(test_func):
+      test_func(text)
+    self.compare_with_solution_or_wait(submission, 'findSingletonWords', comp)
+
+  @graded(is_hidden=True)
+  def test_2(self):
+    """3f-2-hidden:  Random trials (bigger)."""
+    numTokens, numTypes = (10000, 100)
+    import random
+    random.seed(42)
+    text = ' '.join(str(random.randint(0, numTypes)) for _ in range(numTokens))
+    def comp(test_func):
+      test_func(text)
+    self.compare_with_solution_or_wait(submission, 'findSingletonWords', comp)
+class Test_3g(GradedTestCase):
+
+  @graded()
+  def test_0(self):
+    """3g-0-basic:  Simple test cases."""
+    self.assertEqual(0,
+                     submission.computeLongestPalindromeLength(""),
+                     msg='simple test')
+    self.assertEqual(1,
+                     submission.computeLongestPalindromeLength("a"),
+                     msg='simple test')
+    self.assertEqual(2,
+                     submission.computeLongestPalindromeLength("aa"),
+                     msg='simple test')
+    self.assertEqual(1,
+                     submission.computeLongestPalindromeLength("ab"),
+                     msg='simple test')
+    self.assertEqual(3,
+                     submission.computeLongestPalindromeLength("animal"),
+                     msg='simple test')
+
+  @graded(is_hidden=True, timeout=1)
+  def test_1(self):
+    """3g-1-hidden:  Random trials."""
+    import random
+    random.seed(42)
+    # Generate a random string of the given length
+    text = genLimitedCharacterString(2,10)
+    def comp(test_func):
+      test_func(text)
+    self.compare_with_solution_or_wait(submission, 'computeLongestPalindromeLength', comp)
+
+  @graded(is_hidden=True, timeout=1)
+  def test_2(self):
+    """3g-2-hidden:  Random trials (more characters)"""
+    import random
+    random.seed(42)
+    # Generate a random string of the given length
+    text = genLimitedCharacterString(10,10)
+    def comp(test_func):
+      test_func(text)
+    self.compare_with_solution_or_wait(submission, 'computeLongestPalindromeLength', comp)
+
+  @graded(is_hidden=True, timeout=1)
+  def test_3(self):
+    """3g-3-hidden:  Random trials (long)."""
+    import random
+    random.seed(42)
+    # Generate a random string of the given length
+    text = genLimitedCharacterString(5,20)
+    def comp(test_func):
+      test_func(text)
+    self.compare_with_solution_or_wait(submission, 'computeLongestPalindromeLength', comp)
+
+  @graded(is_hidden=True, timeout=2)
+  def test_4(self):
+    """3g-4-hidden:  Random trials (longer)."""
+    import random
+    random.seed(42)
+    # Generate a random string of the given length
+    text = genLimitedCharacterString(5,400)
+    def comp(test_func):
+      test_func(text)
+    self.compare_with_solution_or_wait(submission, 'computeLongestPalindromeLength', comp)
+
+def getTestCaseForTestID(test_id):
+  question, part, _ = test_id.split('-')
+  g = globals().copy()
+  for name, obj in g.items():
+    if inspect.isclass(obj) and name == ('Test_'+question):
+      return obj('test_'+part)
+
+if __name__ == '__main__':
+  # Parse for a specific test
+  parser = argparse.ArgumentParser()
+  parser.add_argument('test_case', nargs='?', default='all')
+  test_id = parser.parse_args().test_case
+
+  assignment = unittest.TestSuite()
+  if test_id != 'all':
+    assignment.addTest(getTestCaseForTestID(test_id))
+  else:
+    assignment.addTests(unittest.defaultTestLoader.discover('.', pattern='grader.py'))
+  CourseTestRunner().run(assignment)
---
+++ src/submission.py
@@ -0,0 +1,109 @@
+import collections
+import math
+
+############################################################
+# Problem 3a
+
+def findAlphabeticallyLastWord(text):
+    """
+    Given a string |text|, return the word in |text| that comes last
+    alphabetically (that is, the word that would appear last in a dictionary).
+    A word is defined by a maximal sequence of characters without whitespaces.
+    You might find max() and list comprehensions handy here.
+    """
+    pass
+    # ### START CODE HERE ###
+    # ### END CODE HERE ###
+
+############################################################
+# Problem 3b
+
+def euclideanDistance(loc1, loc2):
+    """
+    Return the Euclidean distance between two locations, where the locations
+    are pairs of numbers (e.g., (3, 5)).
+    """
+    pass
+    # ### START CODE HERE ###
+    # ### END CODE HERE ###
+
+############################################################
+# Problem 3c
+
+def mutateSentences(sentence):
+    """
+    Given a sentence (sequence of words), return a list of all "similar"
+    sentences.
+    We define a sentence to be similar to the original sentence if
+      - it as the same number of words, and
+      - each pair of adjacent words in the new sentence also occurs in the original sentence
+        (the words within each pair should appear in the same order in the output sentence
+         as they did in the orignal sentence.)
+    Notes:
+      - The order of the sentences you output doesn't matter.
+      - You must not output duplicates.
+      - Your generated sentence can use a word in the original sentence more than
+        once.
+    Example:
+      - Input: 'the cat and the mouse'
+      - Output: ['and the cat and the', 'the cat and the mouse', 'the cat and the cat', 'cat and the cat and']
+                (reordered versions of this list are allowed)
+    """
+    pass
+    # ### START CODE HERE ###
+    # ### END CODE HERE ###
+
+############################################################
+# Problem 3d
+
+def sparseVectorDotProduct(v1, v2):
+    """
+    Given two sparse vectors |v1| and |v2|, each represented as collections.defaultdict(float), return
+    their dot product.
+    You might find it useful to use sum() and a list comprehension.
+    This function will be useful later for linear classifiers.
+    """
+    pass
+    # ### START CODE HERE ###
+    # ### END CODE HERE ###
+
+############################################################
+# Problem 3e
+
+def incrementSparseVector(v1, scale, v2):
+    """
+    Given two sparse vectors |v1| and |v2|, perform v1 += scale * v2.
+    This function will be useful later for linear classifiers.
+    """
+    pass
+    # ### START CODE HERE ###
+    # ### END CODE HERE ###
+
+############################################################
+# Problem 3f
+
+def findSingletonWords(text):
+    """
+    Splits the string |text| by whitespace and returns the set of words that
+    occur exactly once.
+    You might find it useful to use collections.defaultdict(int).
+    """
+    pass
+    # ### START CODE HERE ###
+    # ### END CODE HERE ###
+
+############################################################
+# Problem 3g
+
+def computeLongestPalindromeLength(text):
+    """
+    A palindrome is a string that is equal to its reverse (e.g., 'ana').
+    Compute the length of the longest palindrome that can be obtained by deleting
+    letters from |text|.
+    For example: the longest palindrome in 'animal' is 'ama'.
+    Your algorithm should run in O(len(text)^2) time.
+    You should first define a recurrence before you start coding.
+    """
+    pass
+    # ### START CODE HERE ###
+    # ### END CODE HERE ###
